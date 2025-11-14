2e98986cf2967eb4f20629a73fbd04bb12795c5f
EntropicProblems <155194337+EntropicProblems@users.noreply.github.com>
2025-11-14 12:35:10 -0500

    Causing a bigger conflict

--- samplelist.py
+++ samplelist.py
@@ -1,9 +1,10 @@
 import numpy as np
 array = [1,2,3,4,5,6]
 nparray = np.array(array)
-tolerance = 1
+tolerance = 3
 mean = np.mean(nparray)
 standarddev = np.std(nparray)
 print("Mean = ",mean, "standard deviation = ",standarddev)
 print('\n')
+
 print("range for", tolerance, "deviations = ", mean - standarddev * tolerance, "-", mean + standarddev * tolerance)
